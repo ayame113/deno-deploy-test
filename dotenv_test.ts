1b04f52777831b8c32bc6baea92b354a793cbf84
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-10-24 22:39:28 +0900

    Update dotenv_test.ts

--- dotenv_test.ts
+++ dotenv_test.ts
@@ -1,1 +1,5 @@
 import "https://raw.githubusercontent.com/ayame113/deno-dotenv/deploy-compatibility/load.ts"
+
+import { listenAndServe } from "https://deno.land/std@0.112.0/http/server.ts";
+
+listenAndServe(":8000", () => new Response(Deno.inspect(window)));
