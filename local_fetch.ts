9cbd769e534b7e1cd01b5b948504c94d116305ca
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-11-15 11:34:14 +0900

    Update local_fetch.ts

--- local_fetch.ts
+++ local_fetch.ts
@@ -1,22 +1,26 @@
 console.log("cwd: ", Deno.cwd());
-console.log("import.meta.main: ", import.meta.main);
+console.log("import.meta.url: ", import.meta.url);
+
 try {
   console.log(await Deno.readFile("/src/local_fetch.ts"))
 } catch (e) {
-  console.log("readFile error: ", e)
+  console.log("readFile error: ", e) // Occasionally happens => Error: generic read file error #42
 }
+
 try {
   console.log(await Deno.readTextFile("/src/local_fetch.ts"))
 } catch (e) {
-  console.log("readTextFile error: ", e)
+  console.log("readTextFile error: ", e) // Occasionally happens => Error: generic read file error #42
 }
+
 try {
   console.log(await fetch("/src/local_fetch.ts"))
 } catch (e) {
-  console.log("fetch error: ", e)
+  console.log("fetch error: ", e) // TypeError: Invalid URL
 }
+
 try {
   console.log(await fetch("file:///src/local_fetch.ts"))
 } catch (e) {
-  console.log("fetch error (file plotocol): ", e)
+  console.log("fetch error (file plotocol): ", e) // PermissionDenied: Read access not allowed
 }
