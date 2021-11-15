4e9402199a53dcb15338db3cae065b0d64d81b50
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-11-15 11:24:50 +0900

    Update local_fetch.ts

--- local_fetch.ts
+++ local_fetch.ts
@@ -1,1 +1,17 @@
-
+console.log("cwd: ", Deno.cwd());
+console.log("import.meta.main: ", import.meta.main);
+try {
+  console.log(await Deno.readTextFile("/src/local_fetch.ts"))
+} catch (e) {
+  console.log("readTextFile error: ", e)
+}
+try {
+  console.log(await fetch("/src/local_fetch.ts"))
+} catch (e) {
+  console.log("fetch error: ", e)
+}
+try {
+  console.log(await fetch("file:///src/local_fetch.ts"))
+} catch (e) {
+  console.log("fetch error (file plotocol): ", e)
+}
