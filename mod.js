693ca9274dedb68f8e66dd0e73bb74a62c6dd03c
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-11-23 00:02:51 +0900

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,11 +1,18 @@
 import { serve } from "https://deno.land/std@0.115.1/http/server.ts";
 
-serve((request) => new Response(`import.meta.url: "${import.meta.url}"
-Deno.cwd(): "${Deno.cwd()}"
-Deno.env.get("DENO_REGION"): "${Deno.env.get("DENO_REGION")}"
-Deno.env.get("DENO_DEPLOYMENT_ID"): "${Deno.env.get("DENO_DEPLOYMENT_ID")}"
-Request: ${Deno.inspect(request)}
-window: ${Deno.inspect(window)}
-Deno: ${Deno.inspect(Deno)}
-Deno.core: ${Deno.inspect(Deno.core)}
+serve((request) => new Response(`
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<h3>import.meta.url:</h3><pre>"${import.meta.url}"</pre>
+<h3>Deno.cwd():</h3><pre>"${Deno.cwd()}"</pre>
+<h3>Deno.env.get("DENO_REGION"):</h3><pre>"${Deno.env.get("DENO_REGION")}"</pre>
+<h3>Deno.env.get("DENO_DEPLOYMENT_ID"):</h3><pre>"${Deno.env.get("DENO_DEPLOYMENT_ID")}"</pre>
+<h3>Request:</h3><pre>${Deno.inspect(request)}</pre>
+<h3>window:</h3><pre>${Deno.inspect(window)}</pre>
+<h3>Deno:</h3><pre>${Deno.inspect(Deno)}</pre>
+<h3>Deno.core:</h3><pre>${Deno.inspect(Deno.core)}</pre>
+</body>
+</html>
 `));
