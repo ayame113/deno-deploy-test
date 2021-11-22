f4e44ba6595567dd28f568d6d903b0c62474ff80
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-11-23 00:11:46 +0900

    Update mod.js

--- mod.js
+++ mod.js
@@ -3,7 +3,18 @@ import { serve } from "https://deno.land/std@0.115.1/http/server.ts";
 serve((request) => new Response(`
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+<style>
+body {
+    background-color: bisque;
+}
+pre {
+    background-color: lightgrey;
+    border-radius: 0.5em;
+    box-shadow: inset black 0 0 11px -6px;
+}
+</style>
+</head>
 <body>
 <h3>import.meta.url:</h3><pre>"${import.meta.url}"</pre>
 <h3>Deno.cwd():</h3><pre>"${Deno.cwd()}"</pre>
