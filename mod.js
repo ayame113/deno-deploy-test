ac776258a1b168f8f563148a1b54decb3e995e38
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-01-17 03:09:24 +0900

    Update mod.js

--- mod.js
+++ mod.js
@@ -21,10 +21,10 @@ pre {
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
 </head>
 <body>
-<h3>import.meta.url:</h3><pre>${Deno.inspect(import.meta.url)}</pre>
 <h3>Deno.cwd():</h3><pre>${Deno.inspect(Deno.cwd())}</pre>
 <h3>Deno.env.toObject():</h3><pre>${Deno.inspect(Deno.env.toObject())}</pre>
 <h3>Request:</h3><pre>${Deno.inspect(request)}</pre>
+<h3>import.meta:</h3><pre>${Deno.inspect(import.meta)}</pre>
 <h3>globalThis:</h3><pre>${Deno.inspect(globalThis)}</pre>
 <h3>globalThis.Deno:</h3><pre>${Deno.inspect(globalThis.Deno)}</pre>
 <a href="https://github.com/ayame113/deno-deploy-test" style="text-align: center;display: block;">github repo</a>
