fbde1270eee85311988808eec024a0407d89a788
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-02-16 18:24:50 +0900

    test

--- local_fetch.ts
+++ local_fetch.ts
@@ -1,27 +1,53 @@
-console.log("cwd: ", Deno.cwd());
-console.log("import.meta.url: ", import.meta.url);
+import { serve } from "https://deno.land/std@0.125.0/http/server.ts";
 
-try {
-  console.log(await Deno.readFile("/src/local_fetch.ts"))
-} catch (e) {
-  console.log("readFile error: ", e) // Occasionally happens => Error: generic read file error #42
+// ディレクトリ取得
+const cwd = Deno.cwd();
+console.log("Deno.cwd():", cwd);
+
+const pathToThisFile = import.meta.url;
+console.log("import.meta.url:", pathToThisFile);
+
+// ファイルの中身を取得
+const uint8Array = await Deno.readFile(new URL("./tmp.ts", import.meta.url));
+console.log("Deno.readFile():", uint8Array);
+const decoder = new TextDecoder();
+console.log("Deno.readFile():", decoder.decode(uint8Array));
+
+const content = await Deno.readTextFile(new URL("./tmp.ts", import.meta.url));
+console.log("Deno.readTextFile():", content);
+
+const res = await fetch(new URL("./tmp.ts", import.meta.url));
+console.log(res.ok);
+console.log(res.status);
+console.log(res.statusText);
+if (res.body) {
+  for await (const chunk of res.body) {
+    console.log(chunk);
+  }
+}
+// const textContent = await res.text();
+// const jsonContent = await res.json();
+// const formContent = await res.formData();
+// const blobContent = await res.blob();
+
+// ディレクトリの中身を取得
+for await (const dirEntry of Deno.readDir(new URL("./", import.meta.url))) {
+  console.log(dirEntry);
 }
 
-try {
-  console.log(await Deno.readTextFile("/src/local_fetch.ts"))
-} catch (e) {
-  console.log("readTextFile error: ", e) // Occasionally happens => Error: generic read file error #42
-}
+const fileHandler = await Deno.open(new URL("./tmp.ts", import.meta.url));
+fileHandler.close();
 
-try {
-  console.log(await fetch("/src/local_fetch.ts"))
-} catch (e) {
-  console.log("fetch error: ", e) // TypeError: Invalid URL
-}
+// ファイル情報を取得
+const stat = await Deno.stat(new URL("./tmp.ts", import.meta.url));
+console.log(stat);
 
-try {
-  console.log(await fetch("file:///src/local_fetch.ts"))
-} catch (e) {
-  console.log("fetch error (file plotocol): ", e) // PermissionDenied: Read access not allowed
-}
+const lstat = await Deno.lstat(new URL("./tmp.ts", import.meta.url));
+console.log(lstat);
 
+const realPath = await Deno.realPath(new URL("./tmp.ts", import.meta.url));
+console.log(realPath);
+
+// serve((req) => {
+//   return new Response(req.url);
+// });
